299792aefa806752adb4c172e91ee477e389d570
Martin Toming <58991701+martjn@users.noreply.github.com>
2022-12-15 15:27:34 +0200

    Add files via upload
    “Created the UI with Materialize CSS”

---
+++ app.js
@@ -0,0 +1,12 @@
+// Storage Controller
+
+// Item Controller
+
+const ItemCtrl = (function()) {
+    console.log('ItemCtrl')
+})();
+
+// UI Controller
+const UICtrl = (function()){
+    console.log('UICtrl')
+})();
