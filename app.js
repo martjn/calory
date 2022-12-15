69b9a56bc37a25095de2dcb33d2f4046b660f424
Martin Toming <58991701+martjn@users.noreply.github.com>
2022-12-15 15:45:38 +0200

    “Created Controllers and Data Structure”
    “Created Controllers and Data Structure”

--- app.js
+++ app.js
@@ -2,11 +2,46 @@
 
 // Item Controller
 
-const ItemCtrl = (function()) {
-    console.log('ItemCtrl')
+const ItemCtrl = (function() {
+    // Item Constructor
+    const Item = function(id, name, calories){
+        this.id = id
+        this.name = name
+        this.calories = calories
+    }
+
+    // Data Structure
+    const data = {
+        items: [
+            {id: 0, name: 'Kartul', calories: 300},
+            {id: 1, name: 'Riis', calories: 250},
+            {id: 2, name: 'Hapukapsas', calories: 400}
+        ],
+        total: 0
+    }
+
+    return {
+        logData: function(){
+            return data
+        }
+    }
+
+
 })();
 
 // UI Controller
-const UICtrl = (function()){
+const UICtrl = (function(){
     console.log('UICtrl')
 })();
+
+// App Controller
+const App = (function(ItemCtrl, UICtrl) {
+    return {
+        init: function(){
+            console.log('Initializing App')
+        }
+    }
+})(ItemCtrl, UICtrl);
+
+// Initialize App
+App.init()
